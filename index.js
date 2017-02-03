2bd8d9ac893c4bb5dd3f8003fcf5408f344dce81
Eric Bower <neurosnap@gmail.com>
2017-02-03 15:15:20 -0500

    remove browserify

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
- 'use strict';
+'use strict';
 
-const scopeify = require('./lib');
-const api = require('./lib/api');
-const getCss = require('./lib/get-css');
-const key = require('./lib/css-key');
+var scopeify = require('./lib');
+var api = require('./lib/api');
+var getCss = require('./lib/get-css');
+var key = require('./lib/css-key');
 
 module.exports = scopeify;
 module.exports.api = api;
--- test.js
+++ test.js
@@ -3,7 +3,7 @@
 /* eslint-disable max-len */
 const test = require('tape');
 const autoprefixer = require('autoprefixer');
-const pse = require('./dist/index');
+const pse = require('./index');
 
 const getCss = pse.getCss;
 const scopeifyFn = () => selector => `${selector}_1`;
